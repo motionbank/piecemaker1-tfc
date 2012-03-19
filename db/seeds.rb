f89ab4dd65920cf4c737a7f3eca6833d7ba1cd99
David Kern <nutbits@gmail.com>
2012-03-19 16:00:57 +0100

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 User.create(
-  :login => 'Administrator',
-  :password => 'Administrator',
-  :password_confirmation => 'Administrator',
+  :login => 'admin',
+  :password => 'admin',
+  :password_confirmation => 'admin',
   :role_name => 'group_admin')
 l = Location.create(
   :location => 'Home')
 p = Piece.create(
   :title => 'Default Piece',
-  :short_name => 'DEFAULT',
-  :is_active => false
+  :short_name => 'DEFAULT'
   )  
 SetupConfiguration.create(
   :location_id => l.id,
   :time_zone => 'Berlin',
   :s3_sub_folder => '',
-  :default_piece_id => p.id,
   :file_locations => [])
 
