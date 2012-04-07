06c8c770aa1a01bd32b3d761521c067bf5e59109
David Kern <nutbits@gmail.com>
2012-04-07 08:23:17 +0200

    put precompile

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module Piecemakerlite
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+    config.assets.precompile += ['flowplayer-3.2.6.min.js']
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
