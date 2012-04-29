f79aa3372092e94b6394071bdcb17303f06e8973
David Kern <nutbits@gmail.com>
2012-04-29 16:36:08 +0200

    edit time zone

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,14 @@
 class AccountController < ApplicationController
+  layout 'standard'
   def show
     
+  end
+  def edit
+    @account = @current
+  end
+  def update
+    @account = @current
+    @account.update_attributes(params[:account])
+    redirect_to :action => 'show'
   end
 end
