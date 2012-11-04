6b0fa5daaa1bcc2f059fb965241d210e5f2e8b7b
David Kern <nutbits@gmail.com>
2012-11-04 18:12:19 +0100

    flow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,7 @@
 //= require jquery-periodical
 //= require jquery-jtruncate
 //= require jquery.dataTables
+//= require flowplayer-3.2.11.min
 //= require piecemaker
 //= require capture
 //= require viewer
