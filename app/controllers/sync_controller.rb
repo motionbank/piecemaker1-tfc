483a424a3c63aeba95e1537d5497a958155e21d1
David Kern <nutbits@gmail.com>
2013-05-03 19:15:27 +0200

    sync test

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -3,6 +3,7 @@ class SyncController < ApplicationController
   def run_sync
   uri = URI.parse('http://piecemaker.org/sync/catch_sync')
   c = Command.first.event_data
+  c = Marshal.dump(c)
   post_params = {
     :command => c
   }
@@ -16,7 +17,7 @@ class SyncController < ApplicationController
   @response = http.start {|htt| htt.request(req)}
   end
   def catch_sync
-    obj = JSON.load(params[:command].to_s)
+    @obj = Marshal.load(params[:command])
     render :layout => false
   end
 end
